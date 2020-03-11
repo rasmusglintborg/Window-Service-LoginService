5aae927879a8dcae701532f2e57054a499b3ad2c
= <=>
2020-03-11 16:03:08 +0100

    Add project files.

---
+++ LoginService/LoginHandler.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace LoginService
+{
+    class LoginInfo
+    {
+        public string username = "username";
+        public string password = "password";
+        
+
+    }
+    class LoginHandler
+    {
+        private readonly Task task;
+        private HostControl hostControl;
+        private readonly HttpClient client = new HttpClient();
+
+        public LoginHandler()
+        {
+            task = new Task(Login);
+        }
+
+        private async void Login()
+        {
+            LoginInfo loginInfo = new LoginInfo();
+
+            string json = JsonConvert.SerializeObject(loginInfo);
+            
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var url = "url";
+            var response = await client.PostAsync(url, data);
+            hostControl.Stop();
+        }
+
+        public bool Start(HostControl hostControl)
+        {
+            this.hostControl = hostControl;
+            task.Start();
+            return true;
+        }
+        public bool Stop()
+        {
+            return true;
+        }
+    }
+}
---
+++ LoginService/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace LoginService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x => {
+                x.Service<LoginHandler>(service => {
+                    service.ConstructUsing(loginHandler => new LoginHandler());
+                    service.WhenStarted((loginHandler, hostControl) => loginHandler.Start(hostControl));
+                    service.WhenStopped(loginHandler => loginHandler.Stop());
+
+                });
+                x.RunAsLocalSystem();
+                x.SetServiceName("LoginService");
+                x.SetDisplayName("Login Service(DanishBits)");
+                x.SetDescription("This service logs me in on danishBit to collect juicy rewards");
+
+            });
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+            Console.ReadLine();
+        }
+    }
+}
+
